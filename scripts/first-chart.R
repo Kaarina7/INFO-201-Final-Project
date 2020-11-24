b89e8c8fa998514b0c8d87a78f42050519ce14f8
Kaarina <k1j20@tulleau.com>
2020-11-23 22:51:50 -0800

    Loaded csv dataset into program

---
+++ scripts/first-chart.R
@@ -0,0 +1,5 @@
+library("dplyr")
+library("leaflet")
+
+# read in dataset
+city_data <- read.csv("/home/kaarina/Documents/INFO201/INFO-201-Final-Project/data/zillow-data-median-list-price/data/city_medianlistingprice_allhomes.csv")
