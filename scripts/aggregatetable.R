517140ba714b7ef382c0ee8addc646a94308c00c
rajc90 <rchphekar@live.com>
2020-11-23 16:14:57 -0800

    worked on aggregate table function

--- scripts/aggregatetable.R
+++ scripts/aggregatetable.R
@@ -1,15 +1,54 @@
 library(dplyr)
 library(lintr)
-
-aggregate_table <- function(dataset, column){
-  dataset %>%
-    group_by(column)
+library(reshape2)
+library(tidyr)
+library(tidyverse)
+library(stringr)
+aggregate_table <- function(data, location_type){
+  
+  if(location_type == "State")
+  columns_needed <- data[, grep("201|State", names(data))]
+  x <- data %>%
+    gather(key = "month", value = "list_price", -State) %>%
+    group_by(State) %>%
+    filter(
+      !is.na(list_price)
+    ) %>%
+    filter(str_detect(month, "201")) %>%
+    mutate(
+      list_price = as.numeric(list_price)
+    ) %>%
+    summarise(
+      list_price = mean(list_price, na.rm = TRUE)
+    ) %>%
+    arrange(desc(list_price)) %>%
+    top_n(10)
 }
-data <- read.csv("C:/Users/rchap/Info201/INFO-201-Final-Project/project-data/days-on-zillow/daysonzillow_public_metro.csv")
 
+
+o <- aggregate_table(data, "State")
+data <- read.csv("C:/Users/rchap/Info201/INFO-201-Final-Project/project-data/median-listing-price/City_MedianListingPrice_AllHomes.csv")
+
+
+
+
+
+a <- data[, grep("201|State", names(data))]
 x <- data %>%
-  select(6:7) %>%
-  rowwise() %>%
+  gather(key = "month", value = "list_price", -State) %>%
+  group_by(State) %>%
+  filter(
+    !is.na(list_price)
+  ) %>%
+ filter(str_detect(month, "2011")) %>%
   mutate(
-    c = mean(c(column_f, column_g), na.rm = TRUE)
-  )
+    list_price = as.numeric(list_price)
+  ) %>%
+  summarise(
+    list_price = mean(list_price, na.rm = TRUE)
+  ) %>%
+  arrange(desc(list_price)) %>%
+  top_n(10)
+
+View(mtcars)
+is.vector(data$month)
