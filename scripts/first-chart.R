520538a0a890a0420cee27d598f6f8667c4bb080
Kaarina <k1j20@tulleau.com>
2020-11-24 17:28:34 -0800

    Added labels to city markers

--- scripts/first-chart.R
+++ scripts/first-chart.R
@@ -15,6 +15,11 @@ location_data <- mutate(location_data, "City, State" = paste(city, state_id))
 
 city_location <- dplyr::left_join(city_data, location_data, by = "City, State")
 
+point_labels <- lapply(seq(nrow(city_location)), function(i) {
+  paste0("<p>", city_location[i, "name"], "<p></p>",
+         city_location[i, "city"], "</p><p>")
+})
+
 leaflet(data = city_location) %>%
   addTiles() %>%
   setView(lng = -100, lat = 40, zoom = 3) %>%
@@ -23,4 +28,5 @@ leaflet(data = city_location) %>%
     lng = ~lng,
     radius = 10000,
     stroke = FALSE,
+    label = lapply(point_labels, htmltools::HTML)
   )
