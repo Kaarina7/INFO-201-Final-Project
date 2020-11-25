0bc725f4d318bd717354d84306c791ea78bbd87a
Kaarina <k1j20@tulleau.com>
2020-11-24 16:24:44 -0800

    Found latitudes and longitudes for cities

--- scripts/first-chart.R
+++ scripts/first-chart.R
@@ -1,5 +1,16 @@
 library("dplyr")
 library("leaflet")
+library("ggmap")
 
 # read in dataset
-city_data <- read.csv("/home/kaarina/Documents/INFO201/INFO-201-Final-Project/data/zillow-data-median-list-price/data/city_medianlistingprice_allhomes.csv")
+city_data <- read.csv("/home/kaarina/Documents/INFO201/INFO-201-Final-Project/project-data/median-listing-price/City_MedianListingPrice_AllHomes.csv",
+                      stringsAsFactors = FALSE)
+
+city_data <- mutate(city_data, "City, State" =  paste(RegionName, State))
+
+location_data <- read.csv("/home/kaarina/Documents/INFO201/INFO-201-Final-Project/project-data/uscities.csv",
+                          stringsAsFactors = FALSE)
+
+location_data <- mutate(location_data, "City, State" = paste(city, state_id))
+
+city_location <- dplyr::left_join(city_data, location_data, by = "City, State")
