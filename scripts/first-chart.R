9db25cf1e8b62dc1ac6383830db1fa699b397fd1
Kaarina <k1j20@tulleau.com>
2020-11-26 16:57:39 -0800

    Fine tuned zoom and marker sizes

--- scripts/first-chart.R
+++ scripts/first-chart.R
@@ -1,6 +1,5 @@
 library("dplyr")
 library("leaflet")
-library("ggmap")
 
 # read in dataset
 city_data <- read.csv("/home/kaarina/Documents/INFO201/INFO-201-Final-Project/project-data/median-listing-price/City_MedianListingPrice_AllHomes.csv",
@@ -15,18 +14,20 @@ location_data <- mutate(location_data, "City, State" = paste(city, state_id))
 
 city_location <- dplyr::left_join(city_data, location_data, by = "City, State")
 
+city_location <- mutate(city_location, radius = 0.025 * X2017.09)
+
 point_labels <- lapply(seq(nrow(city_location)), function(i) {
   paste0("<p>", city_location[i, "name"], "<p></p>",
          city_location[i, "city"], "</p><p>")
 })
 
-leaflet(data = city_location) %>%
+city_map <- leaflet(data = city_location) %>%
   addTiles() %>%
-  setView(lng = -100, lat = 40, zoom = 3) %>%
+  setView(lng = -96, lat = 40, zoom = 4) %>%
   addCircles(
     lat = ~lat,
     lng = ~lng,
-    radius = 10000,
+    radius = ~radius,
     stroke = FALSE,
     label = lapply(point_labels, htmltools::HTML)
   )
