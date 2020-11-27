767ddeb9799ac3335e7eec4c3846bdbd06adbb1b
Kaarina <k1j20@tulleau.com>
2020-11-27 12:05:23 -0800

    Changed absolute path to relative one for datasets

--- scripts/first-chart.R
+++ scripts/first-chart.R
@@ -2,12 +2,12 @@ library("dplyr")
 library("leaflet")
 
 # read in dataset
-city_data <- read.csv("/home/kaarina/Documents/INFO201/INFO-201-Final-Project/project-data/median-listing-price/City_MedianListingPrice_AllHomes.csv",
+city_data <- read.csv("../project-data/median-listing-price/City_MedianListingPrice_AllHomes.csv",
                       stringsAsFactors = FALSE)
 
 city_data <- mutate(city_data, "City, State" =  paste(RegionName, State))
 
-location_data <- read.csv("/home/kaarina/Documents/INFO201/INFO-201-Final-Project/project-data/uscities.csv",
+location_data <- read.csv("../project-data/uscities.csv",
                           stringsAsFactors = FALSE)
 
 location_data <- mutate(location_data, "City, State" = paste(city, state_id))
