8a0af61d588d1ac0f7e08a24014b075b6359f40d
takayoshilucas <taka6@uw.edu>
2020-12-04 16:05:35 -0800

    Moved files

--- scripts/app_server.R
+++
@@ -1,9 +0,0 @@
-library(shiny)
-source("./final-scripts/bar_tab.R")
-source("./final-scripts/line_tab.R")
-source("./final-scripts/map_tab.R")
-source("./final-scripts/intro_and_summary.R")
-source("./styles.css") 
-
-server <- function(input, output) {
-}
