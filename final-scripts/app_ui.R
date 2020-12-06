09d4dc7a631d1cf7b109301d58f362646af679c9
rajc90 <rchphekar@live.com>
2020-12-05 23:01:38 -0800

    working on css and ui

--- final-scripts/app_ui.R
+++ final-scripts/app_ui.R
@@ -6,20 +6,22 @@ source("./final-scripts/intro_and_summary.R")
 source("./styles.css") 
 overviewInformation <- tabPanel(
   title = "Overview",
-  tags$h3(
-    id = "SubHeader",
-    "Choice of Domain: "
-  ),
-  p(
-    "We are business, economics, and computational finance majors, and we felt 
+  tags$div(
+    id = "OverviewSection1",
+    tags$h3(
+      id = "SubHeader",
+      "Choice of Domain: "
+    ),
+    p(
+      "We are business, economics, and computational finance majors, and we felt 
     that the field of housing markets intersected all of our major-related 
     interests, so we chose to focus on it for our project. "),
-  tags$h3(
-    id = "SubHeader",
-    "Our Data sets: "
-  ),
-  p(
-    "The data sets we decided to use come from Zillow. 
+    tags$h3(
+      id = "SubHeader",
+      "Our Data sets: "
+    ),
+    p(
+      "The data sets we decided to use come from Zillow. 
     Zillow regularly gathers information about all of the homes listed for sale 
     on their website and makes it publicly  available, and we decided to make 
     use of several pieces of it. The first lists the median housing prices 
@@ -29,19 +31,39 @@ overviewInformation <- tabPanel(
     The second lists the average number of days a listing stays on Zillow 
     by region. This can be used to examine changes in the speed 
     with which houses sell in different areas."
+    ),
+    tags$h3(
+      id = "SubHeader",
+      "Links to Data Sets: "
+    ),
+    tags$a(
+      href = "https://data.world/zillow-data/median-list-price", "Median Price"
+    ),
+    p(
+      
+    ),
+    tags$a(
+      href = "https://data.world/zillow-data/days-on-zillow", "Days on Zillow"
+    )
   ),
-  tags$h3(
-    id = "SubHeader",
-    "Links to Data Sets: "
-  ),
-  tags$a(
-    href = "https://data.world/zillow-data/median-list-price", "Median Price"
-  ),
-  p(
+  tags$div(
+    id = "OverviewSection1",
+    h1("Visualizations: "),
+    tags$h3(
+      id = "SubHeader",
+      "Visualization 1"
+    ),
+    p(
+      "The first visualization is about ....."
+    ),
+    tags$h3(
+      id = "SubHeader",
+      "Visualization 2"
+    ),
+    p(
+      "The second visualization is about ....."
+    ),
     
-  ),
-  tags$a(
-    href = "https://data.world/zillow-data/days-on-zillow", "Days on Zillow"
   )
 )
 
@@ -85,6 +107,7 @@ ui <- fluidPage(
     visual1,
     visual2,
     visual3,
+    conclusions,
     position = "static-top"
   )
 )
--- final-scripts/intro_and_summary.R
+++ final-scripts/intro_and_summary.R
@@ -1,20 +1,21 @@
-intro_ui <- fluidPage(
-  overviewInformation <- tabPanel(
-    title = "Overview",
+overview_ui <- tabPanel(
+  title = "Overview",
+  tags$div(
+    id = "OverviewSection1",
     tags$h3(
-      id = "SubHeader",
-      "Choice of Domain"
-      ),
+      id = "OverviewSubHeader",
+      "Choice of Domain: "
+    ),
     p(
       "We are business, economics, and computational finance majors, and we felt 
     that the field of housing markets intersected all of our major-related 
     interests, so we chose to focus on it for our project. "),
     tags$h3(
-      id = "SubHeader",
-      "Our Data sets"
+      id = "OverviewSubHeader",
+      "Our Data sets: "
     ),
     p(
-    "The data sets we decided to use come from Zillow. 
+      "The data sets we decided to use come from Zillow. 
     Zillow regularly gathers information about all of the homes listed for sale 
     on their website and makes it publicly  available, and we decided to make 
     use of several pieces of it. The first lists the median housing prices 
@@ -26,25 +27,77 @@ intro_ui <- fluidPage(
     with which houses sell in different areas."
     ),
     tags$h3(
-      id = "SubHeader",
-      "Link to Data Set"
+      id = "OverviewSubHeader",
+      "Links to Data Sets: "
     ),
     tags$a(
       href = "https://data.world/zillow-data/median-list-price", "Median Price"
     ),
+    p(
+      
+    ),
     tags$a(
       href = "https://data.world/zillow-data/days-on-zillow", "Days on Zillow"
     )
+  ),
+  tags$div(
+    id = "OverviewSection1",
+    h1("Visualizations: "),
+    tags$h3(
+      id = "OverviewSubHeader",
+      "Visualization 1"
+    ),
+    p(
+      "The first visualization is about ....."
+    ),
+    tags$h3(
+      id = "OverviewSubHeader",
+      "Visualization 2"
+    ),
+    p(
+      "The second visualization is about ....."
+    ),
+    tags$h3(
+      id = "OverviewSubHeader",
+      "Visualization 2"
+    ),
+    p(
+      "The second visualization is about ....."
+    )
   )
-) 
+)
 
-intro_server <-
-  
-  
-  
-
-
-summary_ui <-
-  
-  
-summary_server <- 
+conclusions <- tabPanel(
+  title = "Conclusions",
+  tags$div(
+    tags$h2(
+      "The following conclusions can be found from the various visualizations
+      that were provided"
+    ),
+    tags$h4(
+      id = "ColnclusionSubHeader",
+      "Conclusions from Visualization1"
+    ),
+    p(
+      
+    )
+  ),
+  tags$div(
+    tags$h4(
+      id = "ColnclusionSubHeader",
+      "Conclusions from Visualization3"
+    ),
+    p(
+      
+    )
+  ),
+  tags$div(
+    tags$h4(
+      id = "ColnclusionSubHeader",
+      "Conclusions from Visualization3"
+    ),
+    p(
+      
+    )
+  )
+)
