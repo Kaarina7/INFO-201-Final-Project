e10b24c33439bf9e7dbb55027e9f9048855d1707
rajc90 <rchphekar@live.com>
2020-12-05 21:46:02 -0800

    working on css and ui

--- final-scripts/app_ui.R
+++ final-scripts/app_ui.R
@@ -6,51 +6,86 @@ source("./final-scripts/intro_and_summary.R")
 source("./styles.css") 
 overviewInformation <- tabPanel(
   title = "Overview",
+  tags$h3(
+    id = "SubHeader",
+    "Choice of Domain: "
+  ),
   p(
     "We are business, economics, and computational finance majors, and we felt 
     that the field of housing markets intersected all of our major-related 
-    interests, so we chose to focus on it for our project. The data sets we 
-    decided to use come from Zillow. Zillow regularly gathers information about 
-    all of the homes listed for sale on their website and makes it publicly 
-    available, and we decided to make use of several pieces of it. 
-    The first lists the median housing prices across the nation according to 
-    different regional categories. It can be used to answer questions about how 
-    house prices vary by geographic region, as well as how they've changed over 
-    the past ten years. The second lists the average number of days a listing 
-    stays on Zillow by region. This can be used to examine changes in the speed 
+    interests, so we chose to focus on it for our project. "),
+  tags$h3(
+    id = "SubHeader",
+    "Our Data sets: "
+  ),
+  p(
+    "The data sets we decided to use come from Zillow. 
+    Zillow regularly gathers information about all of the homes listed for sale 
+    on their website and makes it publicly  available, and we decided to make 
+    use of several pieces of it. The first lists the median housing prices 
+    across the nation according to different regional categories. 
+    It can be used to answer questions about how house prices vary by 
+    geographic region, as well as how they've changed over the past ten years. 
+    The second lists the average number of days a listing stays on Zillow 
+    by region. This can be used to examine changes in the speed 
     with which houses sell in different areas."
+  ),
+  tags$h3(
+    id = "SubHeader",
+    "Links to Data Sets: "
+  ),
+  tags$a(
+    href = "https://data.world/zillow-data/median-list-price", "Median Price"
+  ),
+  p(
     
-
+  ),
+  tags$a(
+    href = "https://data.world/zillow-data/days-on-zillow", "Days on Zillow"
   )
 )
-bar <- tabPanel(
+
+
+
+visual1 <- tabPanel(
   title = "Visual 1",
-  h1("This is a chart")
+  tags$h1(
+    id = "Header",
+    "This is a chart"
+    )
 )
 
-map <- tabPanel(
+visual2 <- tabPanel(
   title = "Visual 2",
-  h1("This is a map")
+  tags$h1(
+    id = "Header",
+    "This is a map"
+    )
 )
 
-line <- tabPanel(
+visual3 <- tabPanel(
   title = "Visual 3",
-  h1("This is a chart")
+  tags$h1(
+    id = "SubHeader",
+    "This is a chart"
+    )
 )
 
-
+conclusions <- tabPanel(
+  title = "Conclusions"
+)
 
 
 ui <- fluidPage(
+  includeCSS("style.css"),
   h1(strong("Zillow Housing Information (2010 - 2017)")),
   navbarPage(
     title = "Navigation Bar",
     overviewInformation,
-    bar,
-    map,
-    line,
-    position = "static-top",
-    inverse = TRUE
+    visual1,
+    visual2,
+    visual3,
+    position = "static-top"
   )
 )
 
--- final-scripts/intro_and_summary.R
+++ final-scripts/intro_and_summary.R
@@ -1,22 +1,39 @@
 intro_ui <- fluidPage(
   overviewInformation <- tabPanel(
     title = "Overview",
-    h3("Why we chose the topic?"),
+    tags$h3(
+      id = "SubHeader",
+      "Choice of Domain"
+      ),
     p(
       "We are business, economics, and computational finance majors, and we felt 
     that the field of housing markets intersected all of our major-related 
-    interests, so we chose to focus on it for our project. The data sets we 
-    decided to use come from Zillow."),
+    interests, so we chose to focus on it for our project. "),
+    tags$h3(
+      id = "SubHeader",
+      "Our Data sets"
+    ),
     p(
-    "Zillow regularly gathers information about 
-    all of the homes listed for sale on their website and makes it publicly 
-    available, and we decided to make use of several pieces of it. 
-    The first lists the median housing prices across the nation according to 
-    different regional categories. It can be used to answer questions about how 
-    house prices vary by geographic region, as well as how they've changed over 
-    the past ten years. The second lists the average number of days a listing 
-    stays on Zillow by region. This can be used to examine changes in the speed 
+    "The data sets we decided to use come from Zillow. 
+    Zillow regularly gathers information about all of the homes listed for sale 
+    on their website and makes it publicly  available, and we decided to make 
+    use of several pieces of it. The first lists the median housing prices 
+    across the nation according to different regional categories. 
+    It can be used to answer questions about how house prices vary by 
+    geographic region, as well as how they've changed over the past ten years. 
+    The second lists the average number of days a listing stays on Zillow 
+    by region. This can be used to examine changes in the speed 
     with which houses sell in different areas."
+    ),
+    tags$h3(
+      id = "SubHeader",
+      "Link to Data Set"
+    ),
+    tags$a(
+      href = "https://data.world/zillow-data/median-list-price", "Median Price"
+    ),
+    tags$a(
+      href = "https://data.world/zillow-data/days-on-zillow", "Days on Zillow"
     )
   )
 ) 
