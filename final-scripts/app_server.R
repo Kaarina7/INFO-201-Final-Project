3dffaad2b06eb5afbfee5b02cee7759be874ff06
takayoshilucas <taka6@uw.edu>
2020-12-07 13:35:45 -0800

    Added line graph

--- final-scripts/app_server.R
+++ final-scripts/app_server.R
@@ -11,6 +11,7 @@ server <- function(input, output) {
     bar_graph(city_data, (input$range[1] - 2009):(input$range[2] - 2009),
               input$size_rank)
   })
+  
   output$conclusion1 <- renderPlotly({
     source("aggregate_Table.R")
     change_in_price <- city_data %>%
@@ -28,5 +29,58 @@ server <- function(input, output) {
                                             round(List.Price, 2))),
                stat = "identity", fill = "Blue")
     return(ggplotly(price_change_plot, tooltip = "text"))
+  })
+  
+  output$line_graph <- renderPlot({
+    line_graph <- function(region_type, region_name, state_code = NULL) {
+      # deal with user input and use it to select the proper dataset
+      region_type <- tolower(region_type)
+      region_name <- tolower(region_name)
+      if (region_type == "state") {
+        state_code <- NULL
+        df <- days_listed_state
+        relevant_data <- df[df$RegionName == region_name, ]
+      } else { # region_type is equal to county
+        df <- days_listed_county
+        state_code <- toupper(state_code)
+        df <- filter(df, df[["StateName"]] == state_code)
+        relevant_data <- df[df$RegionName == region_name, ]
+      }
+      # clean the data to make it graph ready
+      relevant_data <- as.data.frame(t(as.matrix(relevant_data)))
+      names(relevant_data) <- as.matrix(relevant_data[1, ])
+      relevant_data <- tibble::rownames_to_column(relevant_data, "Dates")
+      relevant_data <- relevant_data[-1, ]
+      if (region_type == "county") {
+        relevant_data <- relevant_data[-1, ]
+      }
+      relevant_data[[region_name]] <- 
+        as.numeric(relevant_data[[region_name]])
+      
+      # format labels for use in graph
+      season_labels <- paste(rep(c("Winter\n", "Spring\n", "Summer\n",
+                                   "Fall\n"), each = 3), rep(2010:2017, each = 12))
+      for (n in 1:96) {
+        if (n %% 3 != 1) {
+          season_labels[n] <- ""
+        }
+      }
+      
+      # make the graph
+      ggplot(relevant_data, aes(x = Dates, y = .data[[region_name]],
+                                group = 1)) + geom_line() +
+        labs(title = paste0("Average Days a Home is Listed on Zillow in ",
+                            str_to_title(region_name),
+                            ifelse(is.null(state_code), " State",
+                                   paste(" County,", toupper(state_code)))),
+             y = "Number of Days") +
+        scale_x_discrete(labels = season_labels) + geom_point() +
+        theme(axis.text.x = element_text(angle = 90)) +
+        geom_line(color = "blue")
+    }
+    
+    return(
+      line_graph(input$location_type, input$location_name, input$if_state)
+    )
   })
 }
