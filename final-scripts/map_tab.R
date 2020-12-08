371834de466dba3a41521ea50570b814a68e6953
Kaarina <k1j20@tulleau.com>
2020-12-08 13:42:34 -0800

    Removed redundant code

--- final-scripts/map_tab.R
+++ final-scripts/map_tab.R
@@ -22,54 +22,9 @@ location_data <- mutate(location_data, "City, State" = paste(city, state_id))
 city_location <- city_data %>%
   dplyr::left_join(location_data, by = "City, State")
 
-# create main panel for map tab
-map_main_panel <- mainPanel(
-  h1("Interactive Map"),
-  p("This map helps answer the question of how median house prices have changed
-  geographically over time."),
-  leafletOutput("map")
-)
-
-# create side panel for map tab
-map_side_panel <- sidebarPanel(
-  # choose the year to view
-  selectInput(
-    "date",
-    label = "Choose the date",
-    choices = colnames(city_location)[6:98]
-  )
-)
-
-# create tab for map
-map_ui <- tabPanel(
-    title = "Map",
-    sidebarLayout(
-      map_main_panel,
-      map_side_panel  
-    )
-  )
 
 # create labels for map
 point_labels <- lapply(seq(nrow(city_location)), function(i) {
   paste0("<p>", city_location[i, "name"], "<p></p>",
          city_location[i, "city"], "</p><p>")
 })
-
-map_server <- function(input, output) {
-  # render map
-  output$map = renderLeaflet(
-    # create map with details
-    city_map <- leaflet(data = city_location) %>%
-      addTiles() %>%
-      setView(lng = -96, lat = 40, zoom = 4) %>%
-      addCircles(
-        lat = ~lat,
-        lng = ~lng,
-        radius = ~0.025 * city_location[[input$date]],
-        stroke = FALSE,
-        label = lapply(point_labels, htmltools::HTML)
-      )
-  )
-}
-
-shinyApp(ui = map_ui, server = map_server)
