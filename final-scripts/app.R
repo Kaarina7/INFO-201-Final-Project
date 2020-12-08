a3af775e8ba1766f6753efaa0b83ee00c8c8f5dc
takayoshilucas <taka6@uw.edu>
2020-12-08 15:43:37 -0800

    Removed unecessary source lines

--- final-scripts/app.R
+++ final-scripts/app.R
@@ -1,10 +1,4 @@
 library("shiny")
-source("bar-chart-function.R")
-source("line_tab.R")
-source("map_tab.R")
-source("intro_and_summary.R")
-source("style.css")
-
 
 source("app_ui.R")
 source("app_server.R")
